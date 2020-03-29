79fe099626a32b08fd564ec3b8e0e77440707af2
Volodya Leveryev <volodya.leveryev@gmail.com>
2020-03-29 22:25:57 +0900

    Fix minor errors

--- get_matrix.py
+++ get_matrix.py
@@ -1,21 +1,21 @@
-""" Generate matrix of subjects forming education plan """
+""" Генерация матрицы компетенций """
 
 import sys
 import os
 from core import Competence, EducationPlan, Subject
 
 def main(plan_filename: str) -> None:
-    """ Entry point """
+    """ Точка входа """
     plan = EducationPlan(plan_filename)
     competencies = sorted(plan.competence_codes.values(), key=Competence.repr)
     subjects = sorted(plan.subject_codes.values(), key=Subject.repr)
     with open(plan_filename[:-4] + '.txt', mode='w', encoding='UTF-8') as output_file:
-        result = ['']
+        result = ['', '']
         for competence in competencies:
             result.append(competence.code)
         output_file.write('%s\n' % '\t'.join(result))
         for subject in subjects:
-            result = [subject.code]
+            result = [subject.code, subject.name]
             for competence in competencies:
                 if competence.code in subject.competencies:
                     result.append('+')
@@ -25,7 +25,6 @@ def main(plan_filename: str) -> None:
 
 
 if __name__ == '__main__':
-
     if len(sys.argv) != 2:
         print('Usage:\n\tpython {0} <education_plan>.plx'.format(*sys.argv))
         sys.exit()
